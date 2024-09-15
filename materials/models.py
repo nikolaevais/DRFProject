881485a974e9f774ba9846431b95d85f5cf42ab3
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-09-16 02:01:23 +0300

    Task 2. added class Meta and def __str__

--- materials/models.py
+++ materials/models.py
@@ -4,15 +4,28 @@ from users.models import NULLABLE
 
 
 # Create your models here.
-class Сourse():
+class Сourse(models.Model):
     title = models.CharField(max_length=35, verbose_name="Название")
     image = models.ImageField(upload_to="materials/course/image/", verbose_name="Превью", **NULLABLE)
     description = models.TextField(verbose_name="Описание")
 
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
 
-class Lesson():
+    def __str__(self):
+        return f'{self.title} {self.description}'
+
+class Lesson(models.Model):
     title = models.CharField(max_length=35, verbose_name="Название")
-    description =models.TextField(verbose_name="Описание")
+    description = models.TextField(verbose_name="Описание")
     image = models.ImageField(upload_to="materials/lesson/image/", verbose_name="Превью", **NULLABLE)
     link_to_video = models.URLField(verbose_name="Ссылка на видео", **NULLABLE)
-    course = models.ForeignKey(Сourse, on_delete=models.SET_NULL, verbose_name="Курс", **NULLABLE, related_name="couCrse")
+    course = models.ForeignKey(Сourse, on_delete=models.SET_NULL, verbose_name="Курс", **NULLABLE, related_name="course")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return f'{self.title} {self.description}'
