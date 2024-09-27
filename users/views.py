89a497c14bc0a150790c9d9cf77132365270f62b
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-09-27 22:29:20 +0300

    Task 4

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView
@@ -16,4 +17,6 @@ class PaymentListAPIView(ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
+    filter_backends = [filters.OrderingFilter]
     filterset_fields = ('payment_course', 'payment_lesson', 'method_payment')
+    ordering_fields = ['date_payment']
